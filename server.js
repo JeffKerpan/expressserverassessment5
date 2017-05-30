581d8dae5eb1f0d4b386161def661282ddef5543
Jeff Kerpan <jkerpan@gmail.com>
2017-05-30 09:01:09 -0600

    Express CRUD Server functioning

--- server.js
+++ server.js
@@ -10,6 +10,41 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
+app.get('/pets', function (req, res, next) {
+  // This GETS all the pets
+  res.send(req.params);
+});
+
+app.get('/pets/:id', function (req, res, next) {
+  // This GETS a specific pet based on the id
+  res.send(req.params.id);
+});
+
+app.post('/pets', function(req, res, next) {
+  // This CREATES a new pet
+  res.send(req.body);
+});
+
+app.patch('/pets/:id', function(req, res, next) {
+  // This UPDATES a specific pet based on id
+  res.send(req.params.id);
+});
+
+app.delete('/pets/:id', function(req, res, next) {
+  // This DELETES a specific pet based on id
+  res.send(req.params.id);
+});
+
+app.use(function(req, res, next) {
+  // This throws a 404 status code if not found
+  res.sendStatus(404);
+});
+
+app.use(function(err, req, res, next) {
+  // This throws a 500 status code for server error
+  res.sendStatus(500);
+});
+
 app.listen(port, function() {
   console.log('Listening on Port:', + port);
 });
